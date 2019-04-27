9891ea7f58cd6d876838bd5afb3e826a8e329a44
Pawełek <pawelkun@gmail.com>
2019-04-27 15:38:59 +0200

    Added missing changes in _Tile.cs

--- LD44_project/Assets/Scripts/Level_system/Tiles/_Tile.cs
+++ LD44_project/Assets/Scripts/Level_system/Tiles/_Tile.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public abstract class _Tile : _GridElement
+public abstract class _Tile : _GridElement, IHeapItem<_Tile>
 {
     public int gCost;
     public int hCost;
     public int fCost { get => gCost + hCost; }
-    public Dictionary<Agent, _Tile> parents;
+    public Dictionary<Agent, _Tile> lastHops;       // last-hops for specific agent while determining his best path to the taget
+    private int heapIndex;
+
+    public int HeapIndex
+    {
+        get => heapIndex;
+        set => heapIndex = value;
+    }
+
     public abstract bool Walkable { get; }
+
+    public int CompareTo(_Tile tile)
+    {
+        int compare = this.fCost.CompareTo(tile.fCost);
+
+        if (compare == 0)
+            compare = this.hCost.CompareTo(tile.hCost);
+
+        return (-1) * compare;          // we prefer lower costs
+    }
 
 }
