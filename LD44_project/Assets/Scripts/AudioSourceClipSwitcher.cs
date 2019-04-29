59cb6fd74a30f2346d5e369ba0108f10750d2118
Pawełek <pawelkun@gmail.com>
2019-04-29 22:59:51 +0200

    New animators for knight and chesty, new scripts for audio new knight and chesty prefab

---
+++ LD44_project/Assets/Scripts/AudioSourceClipSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceClipSwitcher : MonoBehaviour
+{
+    public List<AudioClip> clips;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void SwitchClip(int n)
+    {
+        audioSource.clip = clips[n];
+    }
+}
---
+++ LD44_project/Assets/Scripts/PlayAudioAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The Audio Source component has an AudioClip option.  The audio
+// played in this example comes from AudioClip and is called audioData.
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudioAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private List<AudioClip> clips;
+    [HideInInspector]
+    public int clipNumber;
+    AudioSource audioData;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        audioData = GetComponent<AudioSource>();
+        audioData.clip = clips[clipNumber];
+        audioData.Play(0);
+    }
+
+    void OnGUI()
+    {
+        
+    }
+}
